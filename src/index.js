a25b12166ce92151d5582d97365bfc06c4db71fe
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-16 10:42:28 +0900

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import firebase from './firebase';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+console.log(firebase);
 
 root.render(
   <StrictMode>
