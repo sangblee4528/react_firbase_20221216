08a2e6257c6dbce4d5034803f39fee9e3f09fe9c
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-16 10:59:43 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Product from './Product';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
       <BrowserRouter>
         <Product />
         <SampleProduct />
