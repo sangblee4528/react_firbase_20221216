28462c0aa1ddab7ecb37f9310f8ce18671cdfff9
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-16 10:41:20 +0900

    1 file modified, 1 added

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import * as firebase from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +14,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// index.js 에서 사용을 위함.
+export default firebase.initializeApp(firebaseConfig);
