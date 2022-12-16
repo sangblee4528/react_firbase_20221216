aa7c2a6cbc6b1a975ac8870adfe8a66494c1393d
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-16 09:37:48 +0900

    1 file added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBC0NlmW7ISJUwQNO1L9J4Q60a3JlNwW4I',
+  authDomain: 'workout-diary-807db.firebaseapp.com',
+  projectId: 'workout-diary-807db',
+  storageBucket: 'workout-diary-807db.appspot.com',
+  messagingSenderId: '835285428288',
+  appId: '1:835285428288:web:1f65bb88e39eed837557a5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
