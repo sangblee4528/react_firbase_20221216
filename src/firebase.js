982db9d3bcf798be6e7c938d5fec06feaa770006
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-16 11:08:57 +0900

    1 file modified

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import * as firebase from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBC0NlmW7ISJUwQNO1L9J4Q60a3JlNwW4I',
-  authDomain: 'workout-diary-807db.firebaseapp.com',
-  projectId: 'workout-diary-807db',
-  storageBucket: 'workout-diary-807db.appspot.com',
-  messagingSenderId: '835285428288',
-  appId: '1:835285428288:web:1f65bb88e39eed837557a5',
+
 };
 
 // Initialize Firebase
