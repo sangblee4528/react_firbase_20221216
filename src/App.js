9c295014414a065c550f1349947ce8a819b84d2e
sangblee4528 <107232071+sangblee4528@users.noreply.github.com>
2022-12-16 10:57:20 +0900

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './style.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SampleProduct from './SampleProduct';
+import Product from './Product';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <firestore />
+      <BrowserRouter>
+        <Product />
+        <SampleProduct />
+      </BrowserRouter>
     </div>
   );
 }
